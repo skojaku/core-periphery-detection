b7077e7a90ad742485480bd39cac3862fcd2d764
Sadamori Kojaku <skojaku@iu.edu>
2021-02-12 23:25:04 -0500

    restring rossa

--- cpnet/Rossa.py
+++ cpnet/Rossa.py
@@ -1,63 +1,45 @@
+import numpy as np
 from scipy.sparse import diags
 from scipy.sparse.linalg import eigs
+
 from . import utils
-from .CPAlgorithm import *
+from .CPAlgorithm import CPAlgorithm
 
 
 class Rossa(CPAlgorithm):
     """Rossa's algorithm for finding continuous core-periphery structure.
 
-    Examples
-    --------
-    Create this object.
-
-    >>> import cpnet
-    >>> rs = cpnet.Rossa()
-
-    **Core-periphery detection**
-
-    Detect core-periphery structure in network G (i.e., NetworkX object):
-
-    >>> rs.detect(G)
-
-    Retrieve the ids of the core-periphery pair to which each node belongs:
-
-    >>> pair_id = rs.get_pair_id()
-
-    Retrieve the coreness:
-
-    >>> coreness = rs.get_coreness()
-
-    .. note::
-
-       This algorithm can accept unweighted and weighted networks.
-       The algorithm assigns all nodes into the same core-periphery pair by construction, i.e., c[node_name] =0 for all node_name.
-
-
-    .. rubric:: Reference
+    This algorithm finds multiple core-periphery pairs in networks.
 
     F. Rossa, F. Dercole, and C. Piccardi. Profiling core-periphery network structure by random walkers. Scientific Reports, 3, 1467, 2013
 
+    .. highlight:: python
+    .. code-block:: python
 
+        >>> import cpnet
+        >>> alg = cpnet.Rossa()
+        >>> alg.detect(G)
+        >>> pair_id = alg.get_pair_id()
+        >>> coreness = alg.get_coreness()
+
+    .. note::
+
+        - [x] weighted
+        - [x] directed
+        - [ ] multiple groups of core-periphery pairs
+        - [ ] continuous core-periphery structure
     """
 
     def __init__(self):
-        return
+        pass
 
     def detect(self, G):
-        """Detect a single core-periphery structure.
+        """Detect core-periphery structure.
 
-        Parameters
-        ----------
-        G : NetworkX graph object
-
-        Examples
-        --------
-        >>> import networkx as nx
-        >>> import cpnet
-        >>> G = nx.karate_club_graph()  # load the karate club network.
-        >>> rs = cp.Rossa()
-        >>> rs.detect(G)
+        :param G: Graph
+        :type G: networkx.Graph or scipy sparse matrix
+        :return: None
+        :rtype: None
         """
         A, nodelabel = utils.to_adjacency_matrix(G)
 
@@ -71,13 +53,23 @@ class Rossa(CPAlgorithm):
         self.qs_ = [Q]
 
     def _score(self, A, c, x):
+        """Calculate the strength of core-periphery pairs.
+
+        :param A: Adjacency amtrix
+        :type A: scipy sparse matrix
+        :param c: group to which a node belongs
+        :type c: dict
+        :param x: core (x=1) or periphery (x=0)
+        :type x: dict
+        :return: strength of core-periphery
+        :rtype: float
+        """
         return [1 - np.mean(x)]
 
     def _detect(self, A):
 
         N = A.shape[0]
         deg = np.array(A.sum(axis=0)).reshape(-1)
-        M = sum(deg) / 2.0
         x = np.zeros((N, 1))
 
         idx = self._argmin2(deg)
@@ -87,7 +79,7 @@ class Rossa(CPAlgorithm):
         bk = 0
         alpha = np.zeros(N)
 
-        for k in range(1, N):
+        for _k in range(1, N):
 
             denom = np.asscalar(np.max([1, np.max(ak * (ak + deg))]))
             score = (2 * ak * (x.T * A) - bk * deg) / denom
