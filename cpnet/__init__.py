29dfa12356de51abfaafa3a0403dc7d78f804ce3
Sadamori Kojaku <skojaku@iu.edu>
2021-02-12 22:10:24 -0500

    update

--- cpnet/__init__.py
+++ cpnet/__init__.py
@@ -8,7 +8,7 @@ from . import utils
 
 from .CPAlgorithm import *
 from .BE import *
-from .MINRES import *
+from .LIP import *
 from .KM_config import *
 from .KM_ER import *
 from .Surprise import *
