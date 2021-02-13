56c5aa32776f8e1fc6606d6b067f9d11279cd0f9
Sadamori Kojaku <skojaku@iu.edu>
2021-02-13 10:18:53 -0500

    change installation script

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 import os
-
 from setuptools import find_packages, setup
 
-__version__ = "0.0.6"
+__version__ = "0.0.16"
+
+def load_requires_from_file(fname):
+    if not os.path.exists(fname):
+        raise IOError(fname)
+    return [pkg.strip() for pkg in open(fname, 'r')]
 
 
 setup(
@@ -13,20 +17,8 @@ setup(
     description="Algorithm for finding multiple core-periphery pairs in networks",
     long_description="Algorithm for finding multiple core-periphery pairs in networks",
     url="https://github.com/skojaku/core-periphery-detection",
-    packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    install_requires=[
-        "networkx>=2.0",
-        "numpy>=1.16.5",
-        "simanneal>=0.4.2",
-        "scipy>=1.5.2",
-        "numba>=0.50.0",
-        "joblib>=0.16.0",
-        "plotly>=4.10.0",
-        "seaborn>=0.11.0",
-        "pandas>=1.1.0",
-        "tqdm>=4.49.0",
-        "fa2",
-    ],
+    packages=find_packages("cpnet"),
+    install_requires=load_requires_from_file('requirements.txt'),
     zip_safe=False,
     include_package_data=True,
     license="MIT",
